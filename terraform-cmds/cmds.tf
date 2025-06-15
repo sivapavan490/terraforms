79835342089e28389936dfd0c05f614049b4b750
sivapavan <sivapavan490@gmail.com>
2025-06-15 09:05:17 +0530

    terraform

---
+++ terraform-cmds/cmds.tf
@@ -0,0 +1,19 @@
+
+``
+terrafrom init
+```
+# It will intailize the reuired plugins to according to provider.
+
+```
+terraform plan
+```
+```
+terraform apply -auto-approve
+```
+```
+terraform destroy -auto-approve
+```
+```
+terraform fmt
+```
+# this cmd will arrange the script in proper order

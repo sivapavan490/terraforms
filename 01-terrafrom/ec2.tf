3f773036824b2784f7e5da7a8a69612c4932096b
sivapavan <sivapavan490@gmail.com>
2025-06-15 08:59:09 +0530

    terrafrom

--- 01-terrafrom/ec2.tf
+++ 01-terrafrom/ec2.tf
@@ -1,27 +1,27 @@
 # By using this terrafrom files we will creatings ec2-instance along with security group.
 
 
-resource "aws_instance" "main"{
-    ami = "ami-0b4f379183e5706b9"
-    vpc_security_group_ids = [aws_security_group.allow_tls.id]
-    instance_type = "t2.micro"
+resource "aws_instance" "main" {
+  ami                    = "ami-0b4f379183e5706b9"
+  vpc_security_group_ids = [aws_security_group.allow_tls.id]
+  instance_type          = "t2.micro"
 }
 
 resource "aws_security_group" "allow_tls" {
-  name = "allow_tls"
+  name        = "allow_tls"
   description = "Allow TLS inbound traffic"
 
   ingress {
-    from_port = 22
-    to_port = 22
-    protocol = "tcp"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-   egress {
-    from_port = 0
-    to_port = 0
-    protocol = "-1" # here -1 represnts all the protocols
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1" # here -1 represnts all the protocols
     cidr_blocks = ["0.0.0.0/0"]
   }
   tags = {
--- 01-terrafrom/provider.tf
+++ 01-terrafrom/provider.tf
@@ -15,7 +15,7 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "6.0.0-beta3"
     }
   }
